252c83e716c900e63bf4fbb161557c4ac819768a
mcoromina <97711918+mcoromina@users.noreply.github.com>
2025-10-31 16:39:42 -0400

    Update subscribe.js

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
   try {
     // Send confirmation email
     await resend.emails.send({
-      from: 'onboarding@resend.dev',
+      from: 'hello@skyscoutsouth.com',
       to: email,
       subject: 'Welcome to SkyScout South! 🎉',
       html: `
